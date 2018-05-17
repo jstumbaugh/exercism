e3cfe9446f5007abc33c5148fac580c7b219a8a0
Jason Stumbaugh <jason.stumbaugh@orm-tech.com>
2018-05-17 13:03:24 -0500

    Finish ruby bob exercise

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,6 +1,6 @@
 class Bob
   def self.hey(remark)
-    if remark.contains_capital_letters? && remark.all_uppercase?
+    if remark.contains_capital_letters?
       "Whoa, chill out!"
     elsif remark.ends_with_questionmark?
       "Sure."
@@ -14,30 +14,17 @@ end
 
 class String
   def contains_capital_letters?
-    !!(self =~ /.*[A-Z].*/)
-  end
-
-  def all_uppercase?
-    self == self.upcase
+    self == self.upcase && self.upcase != self.downcase
   end
 
   def ends_with_questionmark?
-    !!(self =~ /.*\?\s*$/)
+    self.strip[-1] == "?"
   end
 
   def nothing?
-    self.empty? || !!(self =~ /^\s*$/)
+    self.empty? || !!(self =~ /^(\s)+$/)
   end
 end
-
-# Bob answers 'Sure.' if you ask him a question.
-
-# He answers 'Whoa, chill out!' if you yell at him.
-
-# He says 'Fine. Be that way!' if you address him without actually saying
-# anything.
-
-# He answers 'Whatever.' to anything else.
 
 module BookKeeping
   VERSION = 1
